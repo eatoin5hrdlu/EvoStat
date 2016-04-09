27ae7658e38899fc8e929b47852ac4d30d1bef88
Peter Reintjes <peter.reintjes@ncmls.org>
2016-04-09 19:10:43 -0400

    color choice in contrast

--- contrast.py
+++ contrast.py
@@ -1,5 +1,5 @@
-#!C:/cygwin/Python27/python.exe -u
 #!/usr/bin/python -u
+#!C:/cygwin/Python27/python.exe -u
 #!C:/Python27/python -u
 
 from __future__ import print_function
@@ -7,12 +7,13 @@ def plog(str) :
     print(str, file=sys.stderr)
 
 from suppress_stdout_stderr import suppress_stdout_stderr
-import time
+import time, sys
 import numpy as np
 import cv2
 import cv2.cv as cv
 from Tkinter import *
 
+gc_color = 1
 gc_iter = None
 gc_scale = None
 gc_offset = None
@@ -45,14 +46,27 @@ def contrast(image, iter=1, scale=2.0, offset=-80) :
         plog( "img is None after binary threshold in contrast")
     return(img)
 
+def setred() :
+    global gc_color
+    gc_color = 2
+
+def setblue() :
+    global gc_color
+    gc_color = 0
+
+def setgreen() :
+    global gc_color
+    gc_color = 1
+
 def showContrast() :
     """Check contrast algorithm using three values from TkInter console"""
+    global gc_color
     global gc_scale
     global gc_iter
     global gc_offset
     (rval, frame) = gc_camera.read()
     show(frame)
-    greyimage = frame[:,:,1]
+    greyimage = frame[:,:,gc_color]
     s = float(gc_scale.get())/100.0
     o = gc_offset.get()
     i = gc_iter.get()
@@ -77,7 +91,7 @@ def makeTkSliders(contrast) :
     
     scale_label = Label(master,text="Scaling Multiplier")
     scale_label.pack()
-    gc_scale = Scale(master, from_=0, to=200, orient=HORIZONTAL)
+    gc_scale = Scale(master, from_=0, to=300, orient=HORIZONTAL)
     gc_scale.set(int(100*scale))
     gc_scale.pack()
     
@@ -87,14 +101,19 @@ def makeTkSliders(contrast) :
     gc_offset.set(offset)
     gc_offset.pack()
     
+    Button(master, text='Red',  command=setred).pack()
+    Button(master, text='Green', command=setgreen).pack()
+    Button(master, text='Blue', command=setblue).pack()
     Button(master, text='Show', command=showContrast).pack()
     Button(master, text='Quit', command=exit).pack()
     with suppress_stdout_stderr() :
         cv2.namedWindow("camera", cv2.CV_WINDOW_AUTOSIZE)
     if (gc_camera is None) :
         gc_camera = cv2.VideoCapture(0)
+        plog("Opened device 0 for contrast" + str(gc_camera))
     if (gc_camera is None) :
         gc_camera = cv2.VideoCapture(1)
+        plog("Opened device 1 for contrast" + str(gc_camera))
     if (gc_camera is None) :
         plog("No camera available on [0,1]")
         exit(0)
