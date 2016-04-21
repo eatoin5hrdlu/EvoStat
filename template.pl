d11ffeba0b99d80ca295525c58c6fe84e4135f75
Peter Reintjes <peter.reintjes@ncmls.org>
2016-04-21 15:39:22 -0400

    added PID config

--- template.pl
+++ template.pl
@@ -1,19 +1,29 @@
 config( [
-	 updateCycle(20),
-	 debugpause(100),
-         cellstatContrast(2,1.4,-80),
+	 updateCycle(40),
+	 debugpause(10),
+
+         cellstatRegion(80,120,300,350),
+         cellstatContrast(2,1.4,-90),
+	 cellstatHeight(220),   % This sets 100% vessel full line
+
+         lagoonRegion(330,20,510,470),
+         lagoonContrast(2,1.4,-90),
+	 lagoonHeight(180),
+
+	 lagoonWidth(100),
+
+	 numLagoons(4),
+
 	 cellstatScale(1500),
 	 cellstatOffset(100),
-         lagoonContrast(2,1.35,-70),
-	 numLagoons(1),
-         cellstatRegion(90,250,300,290),
-         lagoonRegion(370,40,530,450),
-	 lagoonHeight(130),    % divisor for levelScale
-	 lagoonWidth(60),
+
+	 screen(60,90,point(200,1)),
+         imageSize(580,440),
+
 	 levelScale(140),   % Maximum percentage or mL
 	 levelOffset(10),   % Minimum percentage or mL
-	 frames(100),       % number of frames for lumosity integration
-	 darkness(60),      % Average pixel threshold to identify darkness
+	 frames(100),       % lumosity integration
+	 darkness(60),      % Average pixel thresh dark
 	 camera(outdoor),
 	 rotate(90),
 	 mac(0),  % belongs in snapshot
@@ -24,25 +34,33 @@ config( [
 	 contrast(40),
 	 contrastCmd('/camera_control.cgi?param=2&value='),
 	 picCmd('/snapshot.cgi?resolution=32&user=admin&pwd=lakewould'),
-	 screen(60,90,point(200,1)),
-         imageSize(580,440),
 	 layout([
-		 cellstat(cellstat,below,[od(0.4),temp(37.0),shape(20,7),CF]),
+		 cellstat(cellstatb,below,[od(0.4),temp(37.0),shape(20,8),CF]),
 		 spacer(        x1, next_row, [color(blue)]),
 		 snapshot(     cam, next_row, [ image('mypic1.jpg'),shape(30,30)]),
 		 spacer(        x2, next_row, []),
 		 lagoon( lagoon1, next_row, [temp(35.0), LS, SF]),
 		 lagoon( lagoon2, right,    [temp(35.0), LS, SF]),
 		 lagoon( lagoon3, right,    [temp(34.5) ,LS, SF]),
+		 lagoon( lagoon4, right,    [temp(34.5) ,LS, SF]),
 		 spacer(        x3, next_row, [color(darkgreen)]),
-		 sampler(autosampler, next_row, [shape(40,5),SF])
+		 sampler(autosampler, next_row, [shape(40,6),SF])
                 ])
 	 ]) :-
- LS = shape(24,6),
+ LS = shape(24,7),
 % LF = font(font(times,roman,14)),
  CF = font(font(times,roman,18)),
  SF = font(font(times,roman,20)).
+<
+% BETABOX simulator
+bt_device(cellstatb,     '98:D3:31:90:2B:82').
+bt_device(  lagoon1,     '98:D3:31:70:2A:22').
+bt_device(  lagoon2,     '98:D3:31:40:31:BA').
+bt_device(  autosampler, '98:D3:31:20:2B:EB').
 
+simulator. % Avoid excessive component handshaking
+% logfile(logfile).
+    
 % Simulated devices in the Arduino Mega2560 box
 %
 % bt_device(labcellstat,   '98:D3:31:90:29:0E').
@@ -51,17 +69,27 @@ config( [
 %           '98:D3:31:20:23:4F',
 %           '98:D3:31:70:2B:70',
 %
-% Splatspace simulator
+% BETA
 %bt_device( cellstat,     '98:D3:31:90:2B:82').
 %bt_device(  lagoon1,     '98:D3:31:70:2A:22').
 %bt_device(  lagoon2,     '98:D3:31:40:31:BA').
 %bt_device(  lagoon3,     '98:D3:31:20:2B:EB').
 %
-% Museum simulator
-    
-bt_device( cellstatd,     '98:D3:31:40:90:13').
-bt_device(  lagoond1,     '98:D3:32:30:42:6A').
-bt_device(  lagoond2,     '98:D3:31:30:95:60').
-bt_device(  lagoond3,     '98:D3:31:80:34:39').
-bt_device(autosampler,    '98:D3:31:30:95:4B').
+% Kirkland
+%bt_device( cellstatd,     '98:D3:31:40:90:13').
+%bt_device(  lagoond1,     '98:D3:32:30:42:6A').
+%bt_device(  lagoond2,     '98:D3:31:30:95:60').
+%bt_device(  lagoond3,     '98:D3:31:80:34:39').
+%bt_device(autosampler,    '98:D3:31:30:95:4B').
 
+% pid(Component,
+%     Kp, Ki, Kd, Polarity,
+%     TargetValue, CurrentValue,
+%     Minimum, Maximum, SampleTime)
+
+pid_controllers([
+   pid(cellstat,0.4, 0.3, 0.3, pos, 85, 85, 10, 100, 60000),
+   pid(lagoon1, 0.4, 0.3, 0.3, pos, 30, 30, 10, 100, 60000),
+   pid(lagoon2, 0.4, 0.3, 0.3, pos, 30, 30, 10, 100, 60000),
+   pid(lagoon3, 0.4, 0.3, 0.3, pos, 30, 30, 10, 100, 60000),
+   pid(lagoon4, 0.4, 0.3, 0.3, pos, 30, 30, 10, 100, 60000)]).
