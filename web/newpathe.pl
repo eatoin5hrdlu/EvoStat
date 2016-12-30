c987a14b8d62ea5a89e4b5dd45d0edd33b56faf3
Peter Reintjes <peter.reintjes@ncmls.org>
2016-12-29 19:11:21 -0500

    tweaked file

--- web/newpathe.pl
+++ web/newpathe.pl
@@ -1,7 +1,6 @@
 :- dynamic webok/0.
 :- multifile webok/0.
 
-
 % DCG versions of XPCE get/3, and EvoStat component/3
 
 getx(Obj,ID,[Data|T],T)              :- get(Obj,ID,Data).
@@ -19,6 +18,12 @@ float_tenth(Thing,Obj,[Display|T],T) :-
     DispTemp is float(InTenths)/10.0,
     format(atom(Display), '~4g', [DispTemp]).
 
+label(temperature, Obj) --> ['Temperature '],
+    float_tenths(ttemperature,Obj),
+    [' / '],
+    float_tenths(temperature,Obj),
+    getx(Obj, temperatureUnits).
+
 label(level, Obj) --> getx(Obj,level_t),
                       [' / '],
                       getx(Obj,level), getx(Obj,levelUnits).
@@ -27,93 +32,59 @@ label(od, Obj) --> ['OD',sub(600)],
                    ['  .'], getx(Obj,tturbidity),
                    ['/.'],  getx(Obj,turbidity).
 
-label(temperature, Obj) --> ['Temperature '],
-    float_tenths(ttemperature,Obj),
-    [' / '],
-    float_tenths(temperature,Obj),
-    getx(Obj, temperatureUnits).
-
 label(flow, Obj) --> ['Rate '],
-    getx(Obj,flow_t), [' / '], getx(Obj,flow), getx(Obj,flowUnits).
+                     getx(Obj,flow_t),
+                     [' / '],
+                     getx(Obj,flow), getx(Obj,flowUnits).
 
-label(supply,Name) --> 
-    component(Name,supply,Obj), nl,
-    getx(Obj, level),
-    getx(Obj, levelUnits).
+label(supply,Name) --> component(Name,supply,Obj), nl,
+                       getx(Obj, level),
+                       getx(Obj, levelUnits).
 
-label(cellstat,Name) --> 
-    component(Name,cellstat,Obj), [' '],
-    label(level,Obj),       nl,
-    label(temperature,Obj), nl,
-    label(od, Obj).
+label(cellstat,Name) --> component(Name,cellstat,Obj), [' '],
+                         label(level,Obj),       nl,
+                         label(temperature,Obj), nl,
+                         label(od, Obj).
 
-label(lagoon,Name) --> 
-    component(Name,cellstat,Obj), [' '],
-    label(level,Obj),       nl,
-    label(temperature,Obj), nl,
-    label(flow, Obj).
+label(lagoon,Name) --> component(Name,cellstat,Obj), [' '],
+                       label(level,Obj),       nl,
+                       label(temperature,Obj), nl,
+                       label(flow, Obj).
 
-label(sampler,Name) --> 
-    component(Name,autosampler,Obj), nl,
+label(sampler,Name) -->  component(Name,autosampler,Obj), nl,
     [ 'Next Level Reading in '], getx(Obj,timeleft), ['s'], nl,
     [ 'Next Sample '], getx(Obj,nextsample).
 
 label(drainage,waste) --> ['Waste'].
 
-evostatName(Req,Name) :-
-    memberchk(search(List),Req),
-    memberchk(evostat(Name),List),
-    !.
-evostatName(_,Name) :-
-     gethostname(Fullname),
-     atomic_list_concat([Name|_],'.',Fullname).
-
-backPlate(Name, NamePlate) :-
+newpathe(Req) :-                    % The web page generator
+    gethostname(Fullname),
+    atomic_list_concat([Name|_],'.',Fullname).
     concat_atom(['./images/',Name,'.png'],NamePlate).
-
-web_debug(Req) :-
-    nonvar(Req),
-    logIP(Req),				
-    memberchk(search(Search),Req),
-    memberchk(trace='1',Search),
-    !,
-    ( windows -> 
-      write(user_error,'Tracing in Web page generators broken in windows'),nl(user_error)
-    ; trace
-    ).
-web_debug(_).
-
-newpathe(Req) :-
-  web_debug(Req),
-  evostatName(Req,Name),
-  backPlate(Name,BackPlate),
-  findall(  label([id=S],Supply),
-            label(supply,S,Supply,[]),
-            Nutrient_Inducers),
-  label(cellstat,_,Cellstat,[]),
-  setof(label(id=L,Lagoon),
-        label(lagoon,L,Lagoon,[]),
-	LagoonLabels),
-  label(sampler,autosampler,AutoSamplerLabel,[]),
-  label(drainage,waste,Waste,[]),
-  reply_html_page(
-  [title('Pathe Control Panel'),
-   meta(['http-equiv'(refresh),content(5)],[]),% refresh
-   script([ language(javascript) ],[])],
-   body([background(BackPlate)],
-	[
-          center(a([id=logozone,href('./phagestat.pl')],i(Name))),
-	  div(class=supply,Nutrient_Inducers),
-	  div(class=cellstat, label([],Cellstat)),
-	  div([class=lagoon,width('100%')],LagoonLabels),
-	  div(class=autosampler,label([],AutoSamplerLabel)),
-          div(class=drainage,label([],Waste)),
-          form([class=mod,
-                action='./ipathe.pl'],
-                input([type=submit,name=submit,value=change]))
-        ]
-    )% body
+    findall(  label([id=S],Supply),
+              label(supply,S,Supply,[]),
+              Nutrient_Inducers),
+    label(cellstat,_,Cellstat,[]),
+    setof(label(id=L,Lagoon),
+          label(lagoon,L,Lagoon,[]),
+          LagoonLabels),
+    label(sampler,autosampler,AutoSamplerLabel,[]),
+    label(drainage,waste,Waste,[]),
+    reply_html_page(
+    [title('Pathe Control Panel'),
+     meta(['http-equiv'(refresh),content(5)],[]),% refresh
+     script([ language(javascript) ],[])],
+     body([background(NamePlate)],
+      [center(a([id=logozone,href('./phagestat.pl')],i(Name))),
+       div(class=supply,Nutrient_Inducers),
+       div(class=cellstat, label([],Cellstat)),
+       div([class=lagoon,width('100%')],LagoonLabels),
+       div(class=autosampler,label([],AutoSamplerLabel)),
+       div(class=drainage,label([],Waste)),
+       form([class=mod,
+             action='./ipathe.pl'],
+             input([type=submit,name=submit,value=change]))
+     ])% end of body
   ).
 
-newpathe(Request) :-
- errorPage(Request, 'Error creating EvoStat control page').
+newpathe(Request) :- errorPage(Request,'EvoStat page error').
