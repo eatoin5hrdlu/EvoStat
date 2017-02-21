990ed2708a726a6342b72c9fe4c517f1d01c0756
Peter Reintjes <peter.reintjes@ncmls.org>
2017-02-21 16:30:45 -0500

    added average calculation

--- ptemp.py
+++ ptemp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python -u
 import os, sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 import time
 while( os.stat(sys.argv[1]).st_size < 200 ) :
@@ -14,6 +15,8 @@ for l in f.readlines() :
     list.append(int(l[0]))
     list2.append(int(l[1]))
 llen = len(list)
+print(np.mean(list))
+print(np.mean(list2))
 plt.plot(list)
 plt.plot(list2)
 plt.axis([0,llen,0,100])
