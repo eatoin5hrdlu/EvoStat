d4ffcdf018f65c74a5350f794bcf0470cedd9268
Peter Reintjes <peter.reintjes@ncmls.org>
2017-10-19 15:13:49 -0400

    no cache phagestat image

--- web/phagestat.pl
+++ web/phagestat.pl
@@ -1,12 +1,15 @@
 
+% Problems with image cacheing, we always want to see the latest
 phagestat(_) :-
-  reply_html_page(default,  [
-    title('Inside Phagestat'),
-    meta(['http-equiv'(refresh),content(90)],[]), % Make it an active page
-    script([ language(javascript) ],[])],
-    body([background('./phagestat.jpg')],[])).
+  nocacheHead('Inside Phagestat',NCH),
+  reply_html_page(default, NCH,
+	  body(style('background-color:grey'),
+	       [pre([style='top-margin:10%'],[' ']),
+		center(img([src('./phagestat.jpg'),width('96%')],[])),
+		video([width(320),height(240),autoplay],
+		   source([src('./timelapse.avi'),type('video/x-msvideo')],[]))
+	       ])).
 
 phagestat(Request) :-
     errorPage(Request, 'Error creating PhageStat image').
-
 
