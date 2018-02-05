4192aba24437d5e3531b521e7877fc1327652637
Peter Reintjes <peter.reintjes@ncmls.org>
2018-02-05 15:21:22 -0500

    new toggling software

---
+++ web/ani.pl
@@ -0,0 +1,17 @@
+ani(_Req) :-
+    defaultHead('Evolution Landscape Display',Default),
+    Style = style('body { background-color: #FFFFFF; }'),
+    toggle(Toggle,'label.gif','plain.gif'),
+    Head = [ Style, Toggle | Default ],
+    reply_html_page(Head, body([],
+	[ img([ id(timg),
+		onclick('toggleimage();'),
+		src('/web/images/plain.gif'),width('80%')],[]) ])).
+
+ani(Request) :-
+    errorPage(Request, 'Animation not available').
+
+
+
+
+
---
+++ web/bani.pl
@@ -0,0 +1,16 @@
+bani(_Req) :-
+    defaultHead('Including Mutation Labels',Default),
+    style(Style),
+    toggle(Toggle,'blabel.gif','bplain.gif'),
+    Head = [ Style, Toggle | Default ],
+    reply_html_page(Head, body([],
+	[ img([ id(timg),
+		onclick('toggleimage();'),
+		src('/web/images/bplain.gif'),width('80%')],[]) ])).
+
+bani(Request) :-
+    errorPage(Request, 'Animation not available').
+
+style(style('body { background-color: #000000; }')).
+
+
