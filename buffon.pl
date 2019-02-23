bb5cf8a688abd61b50785da1e2492a52633a9f9d
Peter Reintjes <peter@buffon>
2019-02-23 16:17:13 -0500

    default buffon config

--- buffon.pl
+++ buffon.pl
@@ -1,46 +1,140 @@
-
 config( [
-	 numLagoons(3),
-         imageSize(600,500),
-%         lagoonRegion(250,50,400,520),
-         lagoonRegion(240,36,440,575),
-         cellstatRegion(80,200,240,400),
-	 lagoonHeight(160),    % divisor for levelScale
-	 lagoonWidth(80),
-	 levelScale(140),   % Maximum percentage or mL
-	 levelOffset(10),   % Minimum percentage or mL
+	 textMessages(3600),  % Twice Daily (6min=360 hour=3600 4-hour=14400)
+	 updateCycle(90),    % In seconds
+	 debugpause(10),      % Debug essentially off when pause is 10ms
+	 numLagoons(2),
+         imageSize(580,440),
+%        cellstatRegion(ytop,xleft,ybot,xright),
+         cellstatRegion(130,210,360,260),
+	 cellstatContrast(1, 1.03, -100), % Iterations, Multiply, Subtract
+	 cellstatThreshold(144),
+	 cellstatHeight(230),  % same as 100% of cellstat volume
+
+         lagoonRegion(470,10,620,460),
+         lagoonContrast(  2, 3.5, -50),
+	 lagoonThreshold(110),
+	 lagoonHeight(130),          % same as 100% of lagoon volume
+	 lagoonWidth(40),
+
+	 hostReticule(200,300,400,450),
+	 hostReticuleContrast(1, 1.4, -100),
+	 hostReticuleThreshold(144),
+
 	 frames(100),       % number of frames for lumosity integration
 	 darkness(60),      % Average pixel threshold to identify darkness
-	 camera(outdoor),
-	 rotate(false),
-	 mac(1),  % belongs in snapshot
-	 defaultIP('172.16.3.136'),  % belongs in snapshot
-	 userpwd('&user=scrapsec&pwd=lakewould'),
-	 brightness(11), % 0-240 for indoor camera
-	 brightnessCmd('/camera_control.cgi?param=1&value='),
-	 contrast(40),
-	 contrastCmd('/camera_control.cgi?param=2&value='),
-	 picCmd('/snapshot.cgi?resolution=32&user=admin&pwd=lakewould'),
-	 screen(680, 870, point(750,0)),
+	 camera(0),
+	 rotate(0),
+	 screen(24,85,point(120,1)),
 	 layout([
-		 cellstat(cellstat,below,[od(0.4),temp(37.0),shape(240,80),mac('98:D3:31:20:23:4f'),CF]),
-		 % pumps( pumprail, next_row,   [  mac('98:D3:31:70:2B:70')]),
-%		 pumps( pumprail, next_row,   [  ]),
-		 spacer(        x1, next_row, [color(blue)]),
-		 snapshot(     cam, next_row, [ shape(340,420),image('mypic1.jpg')]),
-		 spacer(        x2, next_row, []),
-		 lagoon( lagoon1, next_row, [temp(35.0), LS, SF]),
-		 lagoon( lagoon2, right,    [temp(35.0), mac('98:D3:31:70:3B:34'),LS, SF]),
-%		 lagoon( lagoon3, right,    [temp(34.5), mac('98:D3:31:20:23:36'),LS, SF]),
-		 lagoon( lagoon3, right,    [temp(34.5),LS, SF]),
-%		 lagoon( lagoon4, right,    [temp(35.0), LS, LF]),
-
-		 spacer(        x3, next_row, [color(darkgreen)]),
-		 sampler(autosampler, next_row, [ shape(400,30),SF])
+		 supply( nutrient, below,  [Supply,levelUnits('L'),v(10)]),
+		 supply( arabinose, right, [Supply,levelUnits(mL)]),
+		 supply( inducer2,  right, [Supply,levelUnits(mL)]),
+		 supply( inducer3,  right, [Supply,levelUnits(mL)]),
+		 cellstat( host0,   below, [tb(400),TL,shape(36,14),CF]),
+		 spacer(     x1, next_row, [color(blue)]),
+		 snapshot(  cam, next_row, [ ]),
+		 spacer(      x2, next_row, []),
+		 lagoon( lagoon1, next_row, [TL, LS, LF]),
+		 lagoon( lagoon2, right,    [TL, LS, LF]),
+		 lagoon( lagoon3, right,   [TL, LS, LF]),
+		 spacer(      x3, next_row, [color(darkgreen)]),
+		 sampler(autosampler, next_row, [shape(40,10),up(45),SF])
                 ])
 	 ]) :-
- LS = shape(180,78),
-% LF = font(font(times,roman,14)),
- CF = font(font(times,roman,18)),
- SF = font(font(times,roman,20)).
+ Supply = shape(11,4),
+ LS = shape(30,14),
+ TL = tl(31),
+ LF = font(font(times,roman,18)),
+ CF = font(font(times,roman,16)),
+ SF = font(font(times,roman,18)).
+
+% When testing with no devices, uncomment next line for fast startup.
+% bt_address(Name, Addr) :- !, fail.
+
+bt_device(host0,   '98:D3:31:FC:1D:75').
+%bt_device(host0,   '98:D3:31:40:31:C3').
+%bt_device(nutrient,   '98:D3:31:30:2A:D1').
+% bt_device( cellstat, '98:D3:31:50:12:F4'). % HC-06 could be bad now
+%bt_device(   host0, '98:D3:31:40:1E:80'). % HC-06 could be bad now
+%NEW bt_device(newcellstat, '98:D3:31:FD:2D:05'). % HC-06 could be bad now
+
+%bt_device(lagoon1,     '98:D3:31:50:14:06').
+%bt_device(lagoon2,    '98:D3:31:80:34:39').
+bt_device(lagoon2,    '98:D3:31:80:46:5F').
+%bt_device( lagoon2,     '98:D3:31:70:2B:70').
+%bt_device(autosampler, '98:D3:32:30:42:6A').
+
+%bt_device(cellstat,    '98:D3:31:70:3B:34'). % Lagoon1 substituted
+%bt_device(cellstat,    '98:D3:31:90:29:0E').
+%bt_device( lagoond2,   '98:D3:31:30:95:60').
+%bt_device( lagoon3,    '98:D3:31:70:3B:2B').
+
+%bt_device(labcellstat,  '98:D3:31:90:29:0E').
+%bt_device(cellstat,     '98:D3:31:90:29:0E').
+%bt_device(  lagoond3,   '98:D3:31:80:34:39'). % was d3
+%bt_device(autosampler,  '98:D3:31:40:1D:D4').
+
+%bt_device(autosamplerY, '98:D3:31:20:23:4F').
+
+% BETA BOX simulator 
+%bt_device( cellstat,     '98:D3:31:90:2B:82').
+%bt_device(  lagoon1,     '98:D3:31:70:2A:22').
+%bt_device(  lagoon2,     '98:D3:31:40:31:BA').
+%bt_device(  autosampler, '98:D3:31:20:2B:EB').
+
+% Museum simulator
+%bt_device( cellstatd,     '98:D3:31:40:90:13').
+%bt_device(  lagoond1,     '98:D3:32:30:42:6A').
+%swbt_device(  lagoond3,     '98:D3:31:80:34:39').
+%bt_device(autosampler,    '98:D3:31:30:95:4B').
+
+% Recipients of texts ( vp = verizon(picture), a = AT&T )
+
+% watcher (Name,  '<carrier> <number>', Hours-per-text)
+
+watcher(reintjes,'vp 9194525098',  4).  % Peter Reintjes
+%watcher(laurie,  'vp 9196987470', 24).   % Laurie Betts
+%watcher(pc,      'a 9193083839',  8).  % The Other Peter
+%watcher(marshall,'a 5056037415', 8).   % Marshall
+%watcher(martha, 'vp 9196024293', 23).  % Martha Collier
+%watcher(lea,    'vp 9194525097', 4).   % Lea
+%watcher(howell, 'vp 7723215578', 48).  % Finn Howell
+
+% Fake Level Data for PID debugging
+% simulator.
+% input(lagoon1, 41).
+
+% pid(Component,
+%     Kp, Ki, Kd, 
+%     Variable, TargetValue,
+%     Minimum, Maximum, SampleTime)
+% Undershoot/Overshoot: Modify Kd and maybe Kp
+% Response too slow:    Increase Ki (Kp?)
+
+deadzone(l,2).  % No change needed if param is this close
+deadzone(t,1).  % Temperature closer than level  
+
+% l = level
+% t = temperature    
+pid_controllers([
+   pid(   host0, 1.0, 0.0, 0.1, l, 32, 10, 100, 120),
+   pid( lagoon2, 1.0, 0.0, 0.1, l, 29, 10, 100, 120)]).
+
+% control(Component, Param, Pos-Ctrl, Alt Component, Neg-Ctrl)
+% For example:    
+%  control(Component, level, InflowTime, Alt-Component, OutflowTime)
+component_valves(   host0,  v0, autosampler, v0).
+component_valves( lagoon1,  v1, autosampler, v1).
+component_valves( lagoon2,  v1, autosampler, v2).
+component_valves( lagoon3,  v1, autosampler, v3).
+component_valves( lagoon4,  v1, autosampler, v4).
+
+    
+%%%%%%%%%%%%%% SYSTEM/USER DEPENDENT STUFF 
+
+% To build stand-alone executable there are different emulators
+:- discontiguous python/1.
+
+python('C:\\Python27\\python.exe') :- windows.
+python('/usr/bin/python')          :- linux.
 
